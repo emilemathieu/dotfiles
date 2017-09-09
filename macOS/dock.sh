309b4bae3b969818d0af0a9cb773491d648c3551
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2017-09-09 22:46:46 +0100

    OSX default settings

---
+++ macOS/dock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/iTerm 2.app"
+dockutil --no-restart --add "/Applications/Atom.app"
+dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/Papers.app"
+dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/WhatsApp.app"
+dockutil --no-restart --add "/Applications/System Preferences.app"
+
+killall Dock
