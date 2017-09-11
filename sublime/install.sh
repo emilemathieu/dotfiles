ba2e6a6d40b0dc1f2af84d58c9772688642b496e
Mathieue <emile.mathieu@stats.ox.ac.uk>
2017-09-11 12:36:07 +0100

    Update install.sh

--- sublime/install.sh
+++ sublime/install.sh
@@ -12,8 +12,6 @@ mkdir -p "$ST3_LOCAL/Installed Packages"
 PKG_CTRL_FILE="$ST3_LOCAL/Installed Packages/Package Control.sublime-package"
 [ ! -f "$PKG_CTRL_FILE" ] && curl -o "$PKG_CTRL_FILE" \
 	"https://sublime.wbond.net/Package Control.sublime-package"
-echo "4"
 
 # Link all sublime-settings files
 ln -sf "$DOTFILES/sublime/User" "$ST3_LOCAL/Packages"
-echo "5"
